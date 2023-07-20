390c5eea92f97755b26652aa5ef2ec501a6e53b1
Емил Стойчев <estallinone@gmail.com>
2023-07-20 17:18:53 +0300

    Add login page with design

--- app/src/components/login/Login.js
+++ app/src/components/login/Login.js
@@ -1,10 +1,28 @@
 import styles from './login.module.css'
 
 const Login = () => {
+
+    const onClickHandler = (e) => {
+        e.preventDefault()
+
+
+    }
     return (
-        <>
-            <h2>Login Page</h2>
-        </>
+        <form className={styles.form}>
+            <div className={styles.divInputs}>
+                <label htmlFor='email'>Email</label>
+                <input type='email' id='email' name='email' placeholder='John@email.com' />
+            </div>
+
+            <div className={styles.divInputs}>
+                <label htmlFor='password'>Password</label>
+                <input type='password' id='password' name='password' placeholder='*******' />
+            </div>
+
+            <button onClick={(e) => onClickHandler(e)}>Login</button>
+
+            <p className={styles.pEl}>or <a href='/register'>Sign up</a></p>
+        </form>
     )
 }
 
