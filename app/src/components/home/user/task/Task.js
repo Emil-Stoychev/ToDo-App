edbb88b813e51936ee3ada5be79dde823fd655ff
Емил Стойчев <estallinone@gmail.com>
2023-08-26 01:01:43 +0300

    only if u are admin can delete task from done section

--- app/src/components/home/user/task/Task.js
+++ app/src/components/home/user/task/Task.js
@@ -281,8 +281,8 @@ export const Task = ({ x, currentTask, setCurrentTask }) => {
                       x?.priority == "low"
                         ? "yellow"
                         : x?.priority == "medium"
-                        ? "blue"
-                        : "red"
+                          ? "blue"
+                          : "red"
                     }
                     xmlns="http://www.w3.org/2000/svg"
                     height="1em"
@@ -357,20 +357,23 @@ export const Task = ({ x, currentTask, setCurrentTask }) => {
                             currentTask?.admins.some(
                               (y) => y?._id == user?._id
                             )) && (
-                            <button onClick={() => onDeleteHandler(x?._id)}>
-                              <svg
-                                fill="white"
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="1em"
-                                viewBox="0 0 512 512"
-                              >
-                                <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
-                              </svg>
-                            </button>
-                          )}
+                              <button onClick={() => onDeleteHandler(x?._id)}>
+                                <svg
+                                  fill="white"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  height="1em"
+                                  viewBox="0 0 512 512"
+                                >
+                                  <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
+                                </svg>
+                              </button>
+                            )}
                         </>
                       )}
                       {x.in == "done" ? (
+                        (currentTask?.admins.some(
+                          (y) => y?._id == user?._id
+                        )) &&
                         <button onClick={() => onMoveHandler(0)}>
                           <svg
                             fill="white"
